be6a71ab5fc3dd0d0956e571d24e795d64907920
Amarnath-Rao <AmarnathRao36@gmail.com>
2023-05-02 00:37:07 +0530

    go

---
+++ hardhat.config.js
@@ -0,0 +1,44 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+const fs = require('fs');
+// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+
+const aKey=fs.readFileSync(".asecret").toString()
+const pKey=fs.readFileSync(".secret").toString()
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${aKey}`,
+      accounts: [pKey]
+    },
+    //goerli: {
+      //url: "",
+      //accounts: [ process.env.privateKey ]
+    //}
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+};
